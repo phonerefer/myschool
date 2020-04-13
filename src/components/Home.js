04f8d51180d4d71714d961ae4bc778e7cc18bc1b
phonerefer <irshadali786143@gmail.com>
2020-04-13 18:27:50 +0530

    Components Created

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-useless-constructor */
+import React , {Component} from 'react';
+
+class Home extends  Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <h1>Hello! World</h1>
+                <p>I am not from this world.</p>
+            </div>
+        );
+    }
+
+}
+export  default Home;
